ca05ac1c47474e662442f0a6b3fa350eb6db1081
dirkbosman <dirkjobosman@gmail.com>
2025-01-08 14:33:48 +0100

    Added apply method

--- Book.scala
+++ Book.scala
@@ -1,18 +1,21 @@
-class Book(val id: Int, val title: String, val author: String){
-    def describe : Unit = println(s"${this.id}; ${this.title}; ${this.author}")
-    def equal(aBook : Book) = (this.id==aBook.id) && (this.title==aBook.title) && (this.author==aBook.author)
+class Book(val id: Int,val title : String,val author : String){
+    def describe : Unit = println(s"${this.id};${this.title};${this.author}")
+    def ==(aBook : Book) = (this.id==aBook.id) && (this.title==aBook.title) && (this.author==aBook.author)
+    def apply(isFrench : Boolean) = if (isFrench) println("This books exists in French") else println("There is not a French version of this book")
 }
-
 object Test extends App{
     val aBook = new Book(42,"La Promesse de l'Aube","Romain Gary")
-    val bBook = new Book(63,"Le Père Goriot","Balzac")
-    aBook.describe
-    bBook.describe
-    println(aBook == bBook) // or println(aBook.equal(bBook))
+    aBook.apply(true)
+    aBook.apply(false)
+
+    // We can also call it in the following way, which makes our object behave like a function:
+    // aBook(true)
+    // aBook(false)
 }
 
-// func_with_scala % scala Book.scala             
-// 42; La Promesse de l'Aube; Romain Gary
-// 63; Le Père Goriot; Balzac
-// false
-// false
+
+// % scala Book.scala             
+// Compiling project (Scala 3.6.2, JVM (23))
+// Compiled project (Scala 3.6.2, JVM (23))
+// This books exists in French
+// There is not a French version of this book
