a3d4282206389ff85bdbaa0de9afd2576c2877f2
dirkbosman <dirkjobosman@gmail.com>
2025-01-08 14:27:43 +0100

    Added readme

--- Book.scala
+++ Book.scala
@@ -1,18 +1,18 @@
-class Book(val id: Int = 0, val pages: Int = 0, val title : String = "Unknown",val author : String = "Unknown"){
-    def describe : Unit = println(s"${this.id};${this.title};${this.author}")
-    def tooLong = this.pages > 250
+class Book(val id: Int, val title: String, val author: String){
+    def describe : Unit = println(s"${this.id}; ${this.title}; ${this.author}")
+    def equal(aBook : Book) = (this.id==aBook.id) && (this.title==aBook.title) && (this.author==aBook.author)
 }
 
 object Test extends App{
-    val aBook = new Book(42,460,"La Promesse de l'Aube","Romain Gary")
+    val aBook = new Book(42,"La Promesse de l'Aube","Romain Gary")
+    val bBook = new Book(63,"Le Père Goriot","Balzac")
     aBook.describe
-
-    if (aBook.tooLong) println(s"The book ${aBook.title} is long.")	
+    bBook.describe
+    println(aBook == bBook) // or println(aBook.equal(bBook))
 }
 
-// Result.....
-// dbosman@dbosmans-MBP func_with_scala % scala Book.scala             
-// Compiling project (Scala 3.6.2, JVM (23))
-// Compiled project (Scala 3.6.2, JVM (23))
-// 42;La Promesse de l'Aube;Romain Gary
-// The book La Promesse de l'Aube is long.
+// func_with_scala % scala Book.scala             
+// 42; La Promesse de l'Aube; Romain Gary
+// 63; Le Père Goriot; Balzac
+// false
+// false
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-Test
+First steps with Scala:
+
+### 1. Install Homebrew (if not already installed)
+
+Open your terminal and run:
+`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"`
+
+After installation, ensure Homebrew is working correctly:
+`brew doctor`
+
+
+### 2. Install Java
+
+Install OpenJDK via Homebrew:
+`brew install openjdk`
+
+Add OpenJDK to your environment:
+`sudo ln -sfn $(brew --prefix openjdk)/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk`
+
+
+### 3. Install Scala
+Install Scala:
+`brew install scala`
+
+Verify the installation:
+`scala -version`
+
+
+### 4. (Optional) Install SBT (Scala Build Tool)
+Install SBT:
+`brew install sbt`
+
+Verify SBT installation:
+`sbt sbtVersion`
+
+
+### 5. General Terminal Commands:
+`% scalac Book.scala`
+`% scala Book.scala`
+`% rm *.class`
+`% rm *.tasty`
