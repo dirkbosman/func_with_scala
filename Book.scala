b510c83ceaffb11b6d51e0db9639116e4cfe5360
dirkbosman <dirkjobosman@gmail.com>
2025-01-08 17:52:06 +0100

    encapsulation -> getter/setters

--- Book.scala
+++ Book.scala
@@ -1,23 +1,37 @@
-// The above program causes an error because we tried to access the id attribute value of the aBook instance 
-// in the Test part. If you remove the last line, the program will work again. However, if we look closely, 
-// we can see the value of id although it is a private field of the Book class. The reason for this is simply 
-// that we are calling the method describe. This method remains accessible outside the Book class. As we access 
-// the id attribute from the Book class, this does not cause an error. If we had put the private keyword to the 
-// describe function, we would have obtained the same error as before.
-
-class Book(private val id: Int, val pages: Int, val title: String, val author: String ){
+// Do not forget to replace val by var. Otherwise you will have an error.
+class Book(private var id: Int, val pages: Int, val title: String, val author: String ){
     def describe : Unit = println(s"${this.id};${this.title};${this.author}")
+    def getId = this.id
+    def setId(new_id: Int): Unit = this.id = new_id
 }
+
+// class Book(private var id_ : Int, val pages: Int, val title: String, val author: String ){
+//     def describe : Unit = println(s"${this.id};${this.title};${this.author}")
+//     def id = this.id_
+//     def id_= (new_id : Int) : Unit = this.id_ = new_id
+// }
+
 
 object Test extends App{
     val aBook = new Book(42,464,"La Promesse de l'Aube","Romain Gary")
     aBook.describe
-    println(aBook.id) // this will through an error due to being protected...s
+    println(aBook.getId)
+    aBook.setId(1954)
+    println(aBook.getId)
 }
 
+// object Test extends App{
+//     val aBook = new Book(42,464,"La Promesse de l'Aube","Romain Gary")
+//     aBook.describe
+//     println(aBook.id)
+//     aBook.id = 1954
+//     println(aBook.id)
+// }
+
+
 // % scala Book.scala             
-// [error] ./Book.scala:8:13
-// [error] value id cannot be accessed as a member of (Test.aBook : Book) from object Test.
-// [error]   private value id can only be accessed from class Book.
-// [error]     println(aBook.id)
-// [error]        
+// Compiling project (Scala 3.6.2, JVM (23))
+// Compiled project (Scala 3.6.2, JVM (23))
+// 42;La Promesse de l'Aube;Romain Gary
+// 42
+// 1954
