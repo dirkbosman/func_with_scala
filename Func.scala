0da5183ac0b90e665fef1d37197d61d5a39f5aa0
dirkbosman <dirkjobosman@gmail.com>
2025-01-09 10:36:08 +0100

    FP -> (1) Higher Order Funcs -> reduce functions

--- Func.scala
+++ Func.scala
@@ -1,69 +1,19 @@
-// The infTen function returns true if the integer given as an argument is less than 10, 
-// so for each element of the collection list, if we get true with infTen at the element 
-// then that element is kept in the list returned by the filter function.
+// Here the reduce function takes the max function which takes two integers as arguments. 
+// This function will be applied to the first two elements of the list which are 1 and 4, 
+// we get 4. This value will be kept and we will compare it with another element of our 
+// list and gradually reduce our initial list to a single value, which will be our maximum.
 
 object Test extends App {
 
-    def infTen(x:Int): Boolean = x < 10 
-    def odd(x:Int): Boolean = x % 2 != 0
-    
-    val collection = List(1, 4, 6, 8, 6, 0, 8, 76, 90, 56, 9, 3)
-    val filter = collection.filter(infTen)
-    val filter_and_sorted = filter.sorted
-    val filter_and_sorted_2 = collection.filter(_ < 10).sorted
+    def max(x:Int,y:Int) = if (x>y) x else y
+    val l = List(1, 4, 6, 8, 6, 0, 8, 76, 90, 56, 9)
 
     println(s"-----"*20)
-    collection.map(println)
-
-    println(s"-----"*20)
-    filter.map(println)
-    
-    println(s"-----"*20)
-    println(filter_and_sorted)
-    println(filter_and_sorted_2)
-
-    println(s"-----"*20)
-    collection.filter(odd).map(println)
-    
-    println(s"-----"*20)
-    collection.filter(odd).sorted.map(println)
-
+    println(l.reduce(max))
 }
 
 // % scala Book.scala
 // Compiling project (Scala 3.6.2, JVM (23))
 // Compiled project (Scala 3.6.2, JVM (23))
 // ----------------------------------------------------------------------------------------------------
-// 1
-// 4
-// 6
-// 8
-// 6
-// 0
-// 8
-// 76
 // 90
-// 56
-// 9
-// 3
-// ----------------------------------------------------------------------------------------------------
-// 1
-// 4
-// 6
-// 8
-// 6
-// 0
-// 8
-// 9
-// 3
-// ----------------------------------------------------------------------------------------------------
-// List(0, 1, 3, 4, 6, 6, 8, 8, 9)
-// List(0, 1, 3, 4, 6, 6, 8, 8, 9)
-// ----------------------------------------------------------------------------------------------------
-// 1
-// 9
-// 3
-// ----------------------------------------------------------------------------------------------------
-// 1
-// 3
-// 9
