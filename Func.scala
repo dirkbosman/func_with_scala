e9fe7a602dfd4bf0b8836ac025f09db5721e6ed1
dirkbosman <dirkjobosman@gmail.com>
2025-01-09 14:15:06 +0100

    FP -> Pattern matching

--- Func.scala
+++ Func.scala
@@ -1,56 +1,29 @@
+import scala.util.Random
+
 object Test extends App {
-    def anotherOne(x:Int) : Unit = {
-        if(x==18) println("Stopped")
-        else{ 
-            println("Another One")
-            anotherOne(x+1)
+    def someQuote: String = {
+        val aList = List(666, 777, 4, 8, 15, 16, 23, 42, 1, 13, 36)
+        val selectedNumber = aList(Random.nextInt(aList.length))
+        val quote = selectedNumber match {
+            case 666 => "I am in hell"
+            case 777 => "I am lucky"
+            case 1 => "This is the life"
+            case 13 => "I am unlucky"
+            case 4 | 8 | 15 | 16 | 23 | 42 => "I am lost"
+            case _ => "I am 36"
         }
-    }
-    anotherOne(0)
-}
-
-object Factorial extends App {
-    def factorial(args: Int): Int = {
-        if (args == 1) {
-            println(s"Returning 1")
-            1
-        } else {
-            val result = args * factorial(args - 1)
-            println(s"Returning $result for args = $args")
-            result
-        }
+        println(s"Selected number: $selectedNumber")
+        quote
     }
 
-    println(s"-------"*20)
-    println(factorial(3))
+    println(someQuote)
+    println(someQuote)
 }
-
-
-object Fibonacci extends App {
-    def fibonacci(x: Int): Int = {
-        if(x < 2) {
-            println(s"Returning $x for x = $x") // This is the log base case
-            println(s"======="*20)
-            x
-        }
-        else {
-            val result = fibonacci(x - 1) + fibonacci(x - 2)
-            println(s"Returning $result for x = $x") // This is the log recursive step
-            println(s"-------"*20)
-            result
-        }
-    }
-    println(s"Final result: ${fibonacci(6)}")
-}
-
-
-
 
 // % scala Func.scala --main-class Factorial
 // Compiling project (Scala 3.6.2, JVM (23))
 // Compiled project (Scala 3.6.2, JVM (23))
-// --------------------------------------------------------------------------------------------------------------------------------------------
-// Returning 1
-// Returning 2 for args = 2
-// Returning 6 for args = 3
-// 6
+// Selected number: 1
+// This is the life
+// Selected number: 4
+// I am lost
